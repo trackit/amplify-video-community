ffd926e938a418f7bc635e8a0543f8ee6ad9c527
florian-vuillemot <florian.vuillemot@posteo.net>
2021-05-24 14:58:00 +0200

    Move views logic from tmp directory to Gatsby page

--- src/tmp/index.ts
+++ src/tmp/index.ts
@@ -1,18 +1,8 @@
 import Dashboard from './Dashboard/Dashboard'
-import VodApp from './VideoOnDemand'
 import VideoPage from './VideoPlayer/VideoPage'
-import HomePage from './Home'
-import LiveApp from './LiveStream'
-import WebinarsApp from './Webinars'
-import SearchPage from './Search'
 
 export {
     Dashboard,
-    VodApp,
     VideoPage,
-    HomePage,
-    LiveApp,
-    WebinarsApp,
-    SearchPage,
 }
 
