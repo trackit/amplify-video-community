ce5f0da09dafa3744f14328e94c8da99d1af0a8f
florian-vuillemot <florian.vuillemot@posteo.net>
2021-06-02 21:46:17 +0200

    Refactor Video Content

--- src/shared/components/VideoSlider/Content.tsx
+++ src/shared/components/VideoSlider/Content.tsx
@@ -5,116 +5,92 @@ import { useEffect, useState } from 'react'
 import { fetchThumbnail } from '../../utilities'
 import { Link } from 'gatsby'
 import { vodAsset } from '../../../models'
-import { Link } from 'gatsby'
 import awsvideoconfig from '../../../aws-video-exports'
 import { VideoPlayer as VideoPlayerComponent } from '../index'
-//import { useHistory } from 'react-router-dom'
 
 const StyledContent = styled.div`
     position: relative;
     height: 38vw;
     margin-top: -40px;
+`
 
-    .__background,
-    .__background__shadow,
-    .__background__image,
-    .__area {
-        position: absolute;
-        top: 0;
-        bottom: 0;
+const Background = styled.div`
+    background: var(--amplify-secondary-color);
+    width: 100%;
+    position: absolute;
+    z-index: 2;
+    top: 0;
+    bottom: 0;
+`
+
+const Area = styled.div`
+    left: 0;
+    right: 0;
+    height: 100%;
+    z-index: 3;
+
+    position: absolute;
+    top: 0;
+    bottom: 0;
+`
+
+const Container = styled.div`
+    padding: 45px 40px;
+    color: wheat;
+    display: flex;
+    flex-direction: row;
+
+    @media (max-width: 1000px) {
+        flex-direction: column;
+        padding: 30px 5px 0px 5px;
     }
+`
 
-    .__background {
-        left: 0;
-        right: 0;
+const CloseButton = styled.button`
+    color: #fff;
+    width: 40px;
+    height: 40px;
+    background: transparent;
+    outline: none;
+    border: none;
+    position: absolute;
+    top: 0;
+    right: 5px;
+`
 
-        &__shadow {
-            left: 0;
-            background: var(--amplify-secondary-color);
-            width: 100%;
-            z-index: 2;
-        }
+const ContentBox = styled.div`
+    flex: 35%;
 
-        &__image {
-            right: 0;
-            width: 70%;
-            height: 100%;
-            background-position: center 10%;
-            background-size: cover;
-            z-index: 1;
-        }
+    @media (max-width: 1000px) {
+        flex: 100%;
     }
+`
 
-    .__area {
-        left: 0;
-        right: 0;
-        height: 100%;
-        z-index: 3;
+const Title = styled(Link)`
+    font-size: var(--amplify-text-lg);
+    color: var(--amplify-secondary-contrast);
+    font-weight: 700;
 
-        &__container {
-            padding: 30px 40px;
-            color: wheat;
-            width: 100%;
-            display: flex;
-            flex-direction: row;
+    &hover {
+        cursor: pointer;
+    }
+`
 
-            &__content {
-                flex: 35%;
+const Description = styled.div`
+    padding-top: 20px;
+    padding-right: 20px;
+    font-size: var(--amplify-text-sm);
+    color: var(--amplify-secondary-contrast);
+    text-align: justify;
+`
 
-                &__title {
-                    font-size: var(--amplify-text-lg);
-                    color: var(--amplify-secondary-contrast);
-                    font-weight: 700;
-                }
+const ContentImage = styled.div`
+    flex: 60%;
+    vertical-align: top;
+    margin-right: 0;
 
-                &__title:hover {
-                    cursor: pointer;
-                }
-
-                &__description {
-                    padding-top: 20px;
-                    padding-right: 20px;
-                    font-size: var(--amplify-text-sm);
-                    color: var(--amplify-secondary-contrast);
-                    text-align: justify;
-                }
-            }
-
-            &__image {
-                flex: 60%;
-                vertical-align: top;
-                margin-right: 0;
-            }
-        }
-
-        @media (max-width: 1000px) {
-            &__container {
-                flex-direction: column;
-                padding: 30px 5px 0px 5px;
-            }
-            &__container__image {
-                display: none;
-            }
-            &__container__content {
-                flex: 100%;
-            }
-        }
-
-        &__close {
-            color: #fff;
-            width: 40px;
-            height: 40px;
-            background: transparent;
-            outline: none;
-            border: none;
-            position: absolute;
-            top: 30px;
-            right: 30px;
-        }
-
-        &__close:hover {
-            cursor: pointer;
-        }
+    @media (max-width: 1000px) {
+        display: none;
     }
 `
 
@@ -150,34 +126,21 @@ const Content = ({ movie, onClose }: ContentProps) => {
 
     return (
         <StyledContent>
-            <div className="__background">
-                <div className="__background__shadow" />
-                <div
-                    className="__background__image"
-                    style={{ backgroundImage: `url(${thumbnailUrl})` }}
-                />
-            </div>
-            <div className="__area">
-                <div className="__area__container">
-                    <div className="__area__container__content">
-                        <Link
-                            to={`/video/${movie.id}`}
-                            className="__area__container__content__title"
-                        >
-                            {movie.title}
-                        </Link>
-                        <div className="__area__container__content__description">
-                            {movie.description}
-                        </div>
-                    </div>
-                    <div className="__area__container__image">
+            <Background />
+            <Area>
+                <Container>
+                    <ContentBox>
+                        <Title to={`/video/${movie.id}`}>{movie.title}</Title>
+                        <Description>{movie.description}</Description>
+                    </ContentBox>
+                    <ContentImage>
                         {<VideoPlayerComponent {...videoJsOptions} />}
-                    </div>
-                </div>
-                <button className="__area__close" onClick={onClose}>
+                    </ContentImage>
+                </Container>
+                <CloseButton onClick={onClose}>
                     <IoClose />
-                </button>
-            </div>
+                </CloseButton>
+            </Area>
         </StyledContent>
     )
 }
