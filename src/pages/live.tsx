52a8116f8c83f1e2c31dc5747fa1596974df7f3d
SPANIER Nathan <spaniernathan67@gmail.com>
2021-06-30 10:16:02 +0200

    Add video player to watch live content

--- src/pages/live.tsx
+++ src/pages/live.tsx
@@ -1,11 +1,43 @@
 import React from 'react'
+import styled from 'styled-components'
 
-import Layout from '../shared/components/Layout/Layout'
+import {
+    VideoPlayer as VideoPlayerComponent,
+    Layout,
+} from '../shared/components'
+
+type VideoPlayerProps = {
+    source: string
+}
+
+const VideoPlayerWrapper = styled.div`
+    background: black;
+`
+
+const VideoPlayer = ({ source }: VideoPlayerProps) => {
+    const videoJsOptions = {
+        autoplay: false,
+        controls: true,
+        sources: [
+            {
+                src: `${source}`,
+                type: 'application/x-mpegURL',
+            },
+        ],
+    }
+    return (
+        <VideoPlayerWrapper>
+            <VideoPlayerComponent {...videoJsOptions} />
+        </VideoPlayerWrapper>
+    )
+}
 
 const LiveApp = () => {
     return (
         <Layout>
-            <p>Coming Soon</p>
+            <VideoPlayer
+                source={'https://dcw3cwpfmf2pb.cloudfront.net/index.m3u8'}
+            />
         </Layout>
     )
 }
