c93c7dcb575c305e784e43b5e788de8a66f26ac0
SPANIER Nathan <spaniernathan67@gmail.com>
2021-07-26 16:55:24 +0200

    Live page updated

--- src/pages/live.tsx
+++ src/pages/live.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Loader from 'react-loader-spinner'
 import styled from 'styled-components'
 
+import { fetchLivestream } from '../shared/utilities'
 import {
-    VideoPlayer as VideoPlayerComponent,
     Layout,
+    VideoPlayer as VideoPlayerComponent,
 } from '../shared/components'
+import { Livestream } from '../models'
 
 type VideoPlayerProps = {
     source: string
@@ -32,14 +35,65 @@ const VideoPlayer = ({ source }: VideoPlayerProps) => {
     )
 }
 
-const LiveApp = () => {
+const LivestreamContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+`
+
+const LivestreamManagement = () => {
+    const [livestream, setLivestream] = useState<Livestream | null>(null)
+    const [loading, setLoading] = useState<boolean>(false)
+
+    useEffect(() => {
+        ;(async () => {
+            setLoading(true)
+            try {
+                const { data } = await fetchLivestream(
+                    '282ee6de-56c3-459c-ad92-64d7a5eaeac5'
+                )
+                setLivestream(data?.getLivestream as Livestream)
+            } catch (error) {
+                console.error(
+                    'admin/livestream/index.tsx(fetchLivestream):',
+                    error
+                )
+            }
+            setLoading(false)
+        })()
+    }, [])
+
     return (
         <Layout>
-            <VideoPlayer
-                source={'https://dcw3cwpfmf2pb.cloudfront.net/index.m3u8'}
-            />
+            {loading ? (
+                <Loader
+                    type="Bars"
+                    color="#FFA41C"
+                    height={100}
+                    width={100}
+                    timeout={3000}
+                />
+            ) : (
+                <LivestreamContainer>
+                    {livestream && (
+                        <>
+                            {livestream.isLive ? (
+                                <div>
+                                    <h1>{livestream.media?.title}</h1>
+                                    <VideoPlayer
+                                        source={livestream.url || ''}
+                                    />
+                                    <p>{livestream.media?.description}</p>
+                                </div>
+                            ) : (
+                                <p>Livestream has not started</p>
+                            )}
+                        </>
+                    )}
+                </LivestreamContainer>
+            )}
         </Layout>
     )
 }
 
-export default LiveApp
+export default LivestreamManagement
