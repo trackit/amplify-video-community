be2663a9671a588c758ca551e8055a9f226386c3
Marlon <marlon.l-huillier@epitech.eu>
2021-07-30 12:07:10 +0200

    refactor(homepage&video): Refactor homepage and videos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,17 @@
-import React, { useRef, useState, useEffect } from 'react'
-import styled, { DefaultTheme } from 'styled-components'
-import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa'
+import React, { useState, useEffect } from 'react'
 
 import { Layout } from '../shared/components'
-import intro from '../videos/introduction-video.mp4'
 import Landing from './homepage/landing'
+import CommunityContent from './homepage/communityContent'
+import IntroductionSection from './homepage/introduction'
+import StreamingContent from './homepage/streaming'
+import Form from './homepage/form'
+import defaultTheme, {
+    NavbarTheme,
+    defaultNavbar,
+} from '../shared/components/theme'
 
-// TODO: Move type and const in other file
-type NavBarTheme = {
-    type: string
-    main: string
-    boxShadow: string
-    contrastText: string
-    amplifyLogo: string
-    amplifyText: string
-    textColor: string
-    textHoverColor: string
-    searchBgColor: string
-    searchMainColor: string
-    searchHoverMainColor: string
-    searchHoverBgColor: string
-    
-    searchTextColor: string
-    searchHoverTextColor: string
-}
-
-const noScrollNavBarTheme: NavBarTheme = {
+const noScrollNavBarTheme: NavbarTheme = {
     type: 'noScroll',
     main: 'rgba(0, 0, 0, 0)',
     boxShadow: 'none',
@@ -37,58 +23,31 @@ const noScrollNavBarTheme: NavBarTheme = {
     searchMainColor: '#ffffff',
     searchHoverMainColor: '#000000',
     searchHoverBgColor: '#ffffff',
-
     contrastText: 'var(--amplify-primary-contrast)',
-    searchTextColor: '#ffffff',
-    searchHoverTextColor: '#000000',
-}
-
-
-const scrollNavBarTheme: NavBarTheme = {
-    type: 'scroll',
-    main: '#ffffff',
-    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.4)',
-    amplifyLogo: 'dark',
-    amplifyText: '#000000',
-    textColor: '#000000',
-    textHoverColor: 'var(--amplify-primary-color)',
-    searchBgColor: 'rgba(0, 0, 0, 0)',
-    searchMainColor: '#000000',
-    searchHoverMainColor: '#ffffff',
-    searchHoverBgColor: 'var(--amplify-primary-color)',
-
-    contrastText: 'var(--amplify-primary-contrast)',
-    searchTextColor: '#ffffff',
-    searchHoverTextColor: '#000000',
-}
-
-const subTheme = {
-    palette: {
-        primary: {
-            main: 'black',
-            contrastText: 'var(--amplify-primary-contrast)',
-            background: '#f2f3f3',
-            ternary: '#dedede',
-            darkblue: '#050029',
-        },
-        navbar: noScrollNavBarTheme,
-        textMd: 'var(--amplify-text-md)',
-    },
 }
 
 const HomePage = () => {
-    const [theme, setTheme] = useState(subTheme)
-    const handleScroll = (e: any) => {
+    const [theme, setTheme] = useState({
+        ...defaultTheme,
+        palette: {
+            ...defaultTheme.palette,
+            navbar: noScrollNavBarTheme,
+        },
+    })
+    const handleScroll = () => {
         if (window.pageYOffset > 20 && theme.palette.navbar.type !== 'scroll') {
             setTheme({
                 ...theme,
                 palette: {
                     ...theme.palette,
-                    navbar: scrollNavBarTheme,
+                    navbar: defaultNavbar,
                 },
             })
         }
-        if (window.pageYOffset <= 20 && theme.palette.navbar.type !== 'noScroll') {
+        if (
+            window.pageYOffset <= 20 &&
+            theme.palette.navbar.type !== 'noScroll'
+        ) {
             setTheme({
                 ...theme,
                 palette: {
@@ -108,9 +67,12 @@ const HomePage = () => {
     })
 
     return (
-        <Layout navBarTheme={theme}>
+        <Layout overrideTheme={theme}>
             <Landing />
-            <div style={{ height: '600px' }} />
+            <IntroductionSection />
+            <StreamingContent />
+            <CommunityContent />
+            <Form />
         </Layout>
     )
 }
