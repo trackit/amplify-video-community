349c7447f043ccd2cc8dfd713507b7d81a326dd9
Vuillemot Florian <florian.vuillemot@posteo.net>
2021-08-21 11:53:11 +0200

    Remove text of video on the landing page

--- src/shared/homepage/landing.tsx
+++ src/shared/homepage/landing.tsx
@@ -1,6 +1,5 @@
 import React, { useRef, useState } from 'react'
-import styled, { css } from 'styled-components'
-import { FaPlayCircle } from 'react-icons/fa'
+import styled from 'styled-components'
 import Modal from 'react-modal'
 
 import intro from '../../videos/introduction-video.mp4'
@@ -19,62 +18,6 @@ const VideoPlayer = styled.video`
     top: 0;
     left: 0;
     z-index: 1;
-`
-
-const TextContainer = styled.div`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    position: relative;
-    top: -100vh;
-    left: 0;
-    z-index: 2;
-`
-
-const TextWrapper = styled.div`
-    margin-left: 120px;
-    width: 33%;
-`
-
-const TextShadow = css`
-    text-shadow: 0px 4px 8px rgba(0, 0, 0, 0.6);
-`
-
-const Title = styled.h1`
-    font-size: 48px;
-    ${TextShadow}
-    color: #ffffff;
-    margin: 0;
-`
-
-const CommonText = css`
-    font-size: 20px;
-    ${TextShadow}
-`
-
-const Text = styled.p`
-    ${CommonText}
-    margin: 70px 0;
-    color: #ffffff;
-`
-
-const OrangeText = styled.span`
-    color: var(--amplify-primary-color);
-`
-
-const PlayButton = styled.button`
-    background-color: rgba(0, 0, 0, 0);
-    border: 0px;
-    display: flex;
-    align-items: center;
-    cursor: pointer;
-`
-
-const ButtonText = styled.span`
-    ${CommonText}
-    color: #ffffff;
-    margin-left: 20px;
 `
 
 const ModalVideoPlayer = styled.video`
@@ -105,12 +48,6 @@ const Landing = () => {
     const [isModalDisplayed, setIsModalDisplayed] = useState(false)
     const videoRef = useRef<HTMLVideoElement>(null)
 
-    const handleDisplayModalVideo = () => {
-        setIsModalDisplayed(true)
-        console.log(videoRef)
-        if (videoRef) videoRef?.current?.pause()
-    }
-
     const handleHideModalVideo = () => {
         setIsModalDisplayed(false)
         if (videoRef) videoRef?.current?.play()
@@ -121,27 +58,6 @@ const Landing = () => {
             <VideoPlayer ref={videoRef} muted loop autoPlay id="amplifyVideo">
                 <source src={intro} type="video/mp4" />
             </VideoPlayer>
-            <TextContainer>
-                <TextWrapper>
-                    <Title>
-                        Fastest, easiest way to build scalable mobile and web
-                        apps.
-                    </Title>
-                    <Text>
-                        Take a tour and find what{' '}
-                        <OrangeText>Amplify Video</OrangeText> has in store for
-                        you!
-                    </Text>
-                    <PlayButton onClick={handleDisplayModalVideo}>
-                        <FaPlayCircle
-                            style={{ filter: 'drop-shadow(2px 2px 2px black)' }}
-                            size="50px"
-                            color="white"
-                        />
-                        <ButtonText>Play introduction video</ButtonText>
-                    </PlayButton>
-                </TextWrapper>
-            </TextContainer>
             <Modal
                 isOpen={isModalDisplayed}
                 onRequestClose={handleHideModalVideo}
