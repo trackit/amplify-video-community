8994f22a5c9985fda47a3326ea360c8d8487cb8a
SPANIER Nathan <spaniernathan67@gmail.com>
2021-08-28 16:30:47 +0200

    90% done reworking the UI, redeployed a new backend to go with UI changes

--- src/pages/videos/index.tsx
+++ src/pages/videos/index.tsx
@@ -57,8 +57,9 @@ const VideoPage = () => {
         }>
     >([])
     const [sections, setSections] = useState<Array<Section> | null>(null)
-    const [nextTokenVodFiles, setNextTokenVodFiles] =
-        useState<string | null>(null)
+    const [nextTokenVodFiles, setNextTokenVodFiles] = useState<string | null>(
+        null
+    )
     const [loadingVodFiles, setLoadingVodFiles] = useState<boolean>(false)
     const [loadingSections, setLoadingSections] = useState<boolean>(false)
     const [haveHighlightedContent, setHaveHighlightedContent] = useState(false)
