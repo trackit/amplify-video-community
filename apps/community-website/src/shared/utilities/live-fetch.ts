8683e12e7ec7097d8c43187b6d57897e79e98afa
SPANIER Nathan <spaniernathan67@gmail.com>
2021-10-04 15:36:34 +0200

    Add fetchLivestreamsWithThumbnail

--- apps/community-website/src/shared/utilities/live-fetch.ts
+++ apps/community-website/src/shared/utilities/live-fetch.ts
@@ -5,9 +5,83 @@ import * as APIt from '../../API'
 import { getAuthMode } from './helper'
 import { listLivestreams, getLivestream } from '../../graphql/queries'
 
+export const listLivestreamsWithThumbnails = /* GraphQL */ `
+  query listLivestreamsWithThumbnails() {
+    listLivestreams() {
+      items {
+        id
+        url
+        isLive
+        createdAt
+        updatedAt
+        media {
+          id
+          title
+          description
+          highlighted
+          source
+          author
+          viewCount
+          createdAt
+          updatedAt
+          thumbnail {
+            id
+            ext
+            src
+            createdAt
+            updatedAt
+          }
+        }
+      }
+    }
+  }
+`
+
 async function fetchLivestreams() {
     return API.graphql({
         query: listLivestreams,
+        authMode: await getAuthMode(),
+    }) as GraphQLResult<APIt.ListLivestreamsQuery>
+}
+
+export type ListLivestreamsWithThumbnailQuery = {
+    listLivestreams?: {
+        __typename: 'ModelLivestreamConnection'
+        items?: Array<{
+            __typename: 'Livestream'
+            id: string
+            url?: string | null
+            isLive?: boolean | null
+            createdAt: string
+            updatedAt: string
+            media?: {
+                __typename: 'Media'
+                id: string
+                title: string
+                description: string
+                highlighted: boolean
+                source?: APIt.Source | null
+                author: string
+                viewCount?: number | null
+                createdAt: string
+                updatedAt: string
+                thumbnail?: {
+                    __typename: 'Thumbnail'
+                    id: string
+                    ext: string
+                    src?: string | null
+                    createdAt: string
+                    updatedAt: string
+                } | null
+            } | null
+        } | null> | null
+        nextToken?: string | null
+    } | null
+}
+
+async function fetchLivestreamsWithThumbnail() {
+    return API.graphql({
+        query: listLivestreamsWithThumbnails,
         authMode: await getAuthMode(),
     }) as GraphQLResult<APIt.ListLivestreamsQuery>
 }
@@ -20,4 +94,4 @@ async function fetchLivestream(id: string) {
     }) as GraphQLResult<APIt.GetLivestreamQuery>
 }
 
-export { fetchLivestreams, fetchLivestream }
+export { fetchLivestreams, fetchLivestream, fetchLivestreamsWithThumbnail }
