47da7223ccec754de5a294bac7050d88f57d0d1d
Julien MEZIERE <julien@trackit.io>
2021-10-12 11:52:15 +0200

    Admin content updated

--- apps/community-website/src/shared/components/Admin/Home.tsx
+++ apps/community-website/src/shared/components/Admin/Home.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import { Card, CardContent, CardHeader } from '@material-ui/core'
+import { Card, CardContent, CardHeader, Link } from '@material-ui/core'
 
 const Home = () => (
     <Card>
         <CardHeader title="Welcome to the administration" />
-        <CardContent>Lorem ipsum sic dolor amet...</CardContent>
+        <CardContent>
+            <img src="https://marmelab.com/react-admin/assets/logo_white.png" />
+        </CardContent>
+        <CardContent>
+            This admin section has been done using the module react-admin, to
+            learn more about it, you can follow the{' '}
+            <Link href="https://marmelab.com/react-admin/Readme.html">
+                tutorial
+            </Link>{' '}
+            on their website.{' '}
+        </CardContent>
     </Card>
 )
 
